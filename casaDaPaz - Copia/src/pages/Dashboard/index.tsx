69c5c562244728f0bde9a0fa99abef0e9398da92
BoneYuuki <138173268+Bianc4Gabri3la@users.noreply.github.com>
2024-10-08 22:01:44 -0300

    Add files via upload

--- casaDaPaz - Copia/src/pages/Dashboard/index.tsx
+++ casaDaPaz - Copia/src/pages/Dashboard/index.tsx
@@ -27,12 +27,18 @@ export default function Dashboard() {
         <>
             <LayoutDashboard>
                 <h1>Casa da Paz</h1>
+                <h2>Sobre Nós</h2>
+                A Associação Assistencial e Promocional Casa da Paz é uma entidade sem fins lucrativos que oferece apoio integral a crianças e adolescentes no município de Umuarama, Paraná. Mantida por doações, parcerias, convênios e voluntários, nossa missão é garantir um ambiente seguro e acolhedor.
                 {<img
-                    src="/imagens/imgsobrenos.jpg"  // caminho da sua imagem
+                    src="/imagens/imgsobrenos.jpg"
                     alt="Descrição da Imagem"
-                    width={500}  // ajuste conforme o tamanho desejado
-                    height={300}  // ajuste conforme o tamanho desejado
+                    width={500}
+                    height={300}
                 />}
+                <br />
+                Fundada para responder à urgência da alimentação, a Casa da Paz rapidamente percebeu que a questão era mais ampla. Observamos que muitas crianças estavam vulneráveis nas ruas, enfrentando altos índices de repetência escolar e evasão, e expostas a perigos graves como o tráfico de drogas e a prostituição.
+
+                Nosso objetivo é proporcionar um ambiente seguro e acolhedor, onde cada jovem possa ter acesso a oportunidades de aprendizado, apoio psicossocial e atividades enriquecedoras. Trabalhamos para oferecer um futuro melhor, garantindo que nossas crianças e adolescentes tenham as ferramentas necessárias para crescer com dignidade e esperança.
 
             </LayoutDashboard>
         </>
